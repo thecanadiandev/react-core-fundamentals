cafd989a8a46130e6568e240f662ce42caec3409
Hari <164977095+thecanadiandev@users.noreply.github.com>
2026-01-05 20:44:55 -0500

    commit 28: redux intro

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import ProductProvider from "./context/product/ProductProvider.jsx";
 import CartProvider from "./context/cart/CartProvider.jsx";
+import "./store/counter";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
